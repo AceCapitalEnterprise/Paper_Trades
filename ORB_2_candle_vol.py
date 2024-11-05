ded1ed104a6b51accdfbf9c890683d322d366816
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-11-05 13:50:03 +0530

    Update ORB_2_candle_vol.py

--- ORB_2_candle_vol.py
+++ ORB_2_candle_vol.py
@@ -9,13 +9,14 @@ import pandas as pd
 import time
 from datetime import datetime, time as t
 import csv
-from breeze_connect import BreezeConnect
+# from breeze_connect import BreezeConnect
+from breez2 import *
 import logging
 
 # Initialize ICICI Breeze API
-breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
-breeze.generate_session(api_secret="9331K77(I8_52JG2K73$5438q95772j@",
-                        session_token="48857404")
+# breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
+# breeze.generate_session(api_secret="9331K77(I8_52JG2K73$5438q95772j@",
+#                         session_token="48857404")
 
 # Define trading parameters
 Call_Buy = None
