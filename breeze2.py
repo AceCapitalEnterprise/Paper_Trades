9c7d2e436e70f0f5e2c4a4ba11ee9ed47afeff12
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-11-05 13:47:18 +0530

    Create breeze2.py

---
+++ breeze2.py
@@ -0,0 +1,7 @@
+from breeze_connect import BreezeConnect
+import logging
+
+# Initialize ICICI Breeze API
+breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
+breeze.generate_session(api_secret="9331K77(I8_52JG2K73$5438q95772j@",
+                        session_token="48857404")
