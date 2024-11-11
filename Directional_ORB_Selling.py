3178e79bd3e9dbc973b03b26dd9b61b5c6a4bdf5
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-11-11 09:40:28 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -1,8 +1,8 @@
 from breeze_connect import BreezeConnect
-breeze = BreezeConnect(api_key="64mV026553514z5565%7S258@^4l3753")
+breeze = BreezeConnect(api_key="d96783Qp368558*55FI36Z24W0ET39Lf")
 import urllib
-breeze.generate_session(api_secret="20286x551)P23443543722J0t28s90D3",
-                        session_token="48941963")
+breeze.generate_session(api_secret="58836g597W4l8977h7~%eX9967^807x3",
+                        session_token="49030448")
 
 
 import numpy as np
