585c5061fbf89cc91063c875f4d1a681bb10df3c
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-11-13 09:23:29 +0530

    Update paper_trade_BN_macd.py

--- paper_trade_BN_macd.py
+++ paper_trade_BN_macd.py
@@ -15,187 +15,209 @@ import csv, re, time, math
 time_1 = t(9,15)
 time_2 = t(15,30)
 
-expiry = '2024-11-13'
+expiry='13-Nov-2024'
+today = datetime.now().strftime('%Y-%m-%d')
+yesterday = (datetime.now() - timedelta(days=5)).strftime('%Y-%m-%d')
 order = 0
-#qty = 25
+stoploss=15
+exit=''
 
-while True:
-    now = datetime.now()
-    if (time_1 < t(datetime.now().time().hour, datetime.now().time().minute) < time_2) and (now.second == 0):
-        today = datetime.now().strftime('%Y-%m-%d')
-        yesterday = (datetime.now() - timedelta(days=5)).strftime('%Y-%m-%d')
+last_row=None
+one_tick=None
+previous_tick=None
+breeze.ws_connect()
+
+def on_ticks(ticks):
+    global one_tick
+    print("-------------------------------------------------------------")
+    one_tick=ticks
+
+    print(ticks)
+
+
+breeze.on_ticks = on_ticks
+
+
+
+def initiate_ws(atm,right=''):
+    if right=='call':    
+        # print("hello")
+        leg=breeze.subscribe_feeds(exchange_code="NFO",
+                                stock_code="CNXBAN",
+                                product_type="options",
+                                # expiry_date=f'{expiry}T06:00:00.000Z',
+                                expiry_date=expiry,
+                                right="call",
+                                strike_price=str(atm),
+                                get_exchange_quotes=True,
+                                get_market_depth=False)
+        print(leg)
+    elif right=='put':
+        leg2=breeze.subscribe_feeds(exchange_code="NFO",
+                                stock_code="CNXBAN",
+                                product_type="options",
+                                expiry_date=expiry,
+                                right="put",
+                                strike_price=str(atm),
+                                get_exchange_quotes=True,
+                                get_market_depth=False)
+        print(leg2)
         
-        
-        data = breeze.get_historical_data_v2(interval="1minute",
-                                                from_date= f"{yesterday}T07:00:00.000Z",
-                                                to_date= f"{today}T17:00:00.000Z",
-                                                stock_code="CNXBAN",
-                                                exchange_code="NSE",
-                                                product_type="cash")
-                
-        
+
+def deactivate_ws(atm,right=''):
+    if right=='call':    
+        breeze.unsubscribe_feeds(exchange_code="NFO",
+                            stock_code="CNXBAN",
+                            product_type="options",
+                            expiry_date=expiry,
+                            right="call",
+                            strike_price=str(atm),
+                            get_exchange_quotes=True,
+                            get_market_depth=False)
+    elif right=='put':
+        breeze.unsubscribe_feeds(exchange_code="NFO",
+                            stock_code="CNXBAN",
+                            product_type="options",
+                            expiry_date=expiry,
+                            right="put",
+                            strike_price=str(atm),
+                            get_exchange_quotes=True,
+                            get_market_depth=False)
+
+# def save_to_csv(path,mode,df):
+
+def adjust_trailing_sl(premium, sl, order):
+    """Adjust the trailing stop-loss based on the current price and factor."""
+    if order in [1, -1]:
+        new_sl = premium - 15 
+        return max(new_sl, sl)
+
+
+def one_minute_data():
+
+    data =breeze.get_historical_data_v2(interval="1minute",
+                                        from_date=f'{yesterday}T07:00:00.000Z',
+                                        to_date=f'{today}T17:00:00.000Z',
+                                        stock_code='CNXBAN',
+                                        product_type='CASH',
+                                        exchange_code='NSE')
+    if 'Success' in data:
         olhc = data['Success']
-        olhc = pd.DataFrame(olhc)
-        olhc['datetime'] = pd.to_datetime(olhc['datetime'])
+        olhc=pd.DataFrame(olhc)
+        olhc['datetime']=pd.to_datetime(olhc['datetime'])
         olhc = olhc[(olhc['datetime'].dt.time >= pd.to_datetime('09:15').time()) &
-                       (olhc['datetime'].dt.time <= pd.to_datetime('15:29').time())]
-        
-        olhc['12_EMA'] = olhc['close'].ewm(span=12, adjust=False).mean()
-        olhc['26_EMA'] = olhc['close'].ewm(span=26, adjust=False).mean()
-        olhc['MACD_Line'] = olhc['12_EMA'] - olhc['26_EMA']
-        olhc['Signal_Line'] = olhc['MACD_Line'].ewm(span=9, adjust=False).mean()
-        olhc['MACD_Histogram'] = olhc['MACD_Line'] - olhc['Signal_Line']
-        olhc['MACD'] = olhc['MACD_Line']
-        
+                (olhc['datetime'].dt.time <= pd.to_datetime('15:29').time())]
+        olhc['12_EMA']=olhc['close'].ewm(span=12,adjust=False).mean()
+        olhc['26_EMA']=olhc['close'].ewm(span=26,adjust=False).mean()
+        olhc['MACD_Line']=olhc['12_EMA']-olhc['26_EMA']
+        olhc['Signal_Line']=olhc['MACD_Line'].ewm(span=9,adjust=False).mean()
+        olhc['Histogram']=olhc['MACD_Line']-olhc['Signal_Line']
+        olhc['MACD']=olhc['MACD_Line']
+
         olhc['close'] = pd.to_numeric(olhc['close'])
         olhc.ta.rsi(close='close', length=14, append=True)
-        
-        last_row = olhc.iloc[-1]
-        second_last = olhc.iloc[-2]
-        third_last = olhc.iloc[-3]
-        
-        
-        if order == 0 :
-            if last_row['RSI_14']>70 and second_last['RSI_14']>70 and last_row['MACD']>second_last['MACD'] and second_last['MACD']>third_last['MACD'] :
-                entry_time = datetime.now().strftime('%H:%M:%S')
-                order = 1
+
+        return olhc
+    
+while True:
+    now = datetime.now()
+    
+    if (time_1 < t(datetime.now().time().hour, datetime.now().time().minute) < time_2) and (now.second == 0):
+        olhc=one_minute_data()
+
+        last_row=olhc.iloc[-1]
+        second_last=olhc.iloc[-2]
+        third_last=olhc.iloc[-3]
+
+        if order==0:
+            if last_row['RSI_14']>=70 and second_last['RSI_14']>=70 and last_row['MACD']>second_last['MACD']>third_last['MACD']:
+                entry_time=datetime.now().strftime('%H:%M:%S')
+                order=1
                 atm = round(last_row['close'] / 100) * 100
-                j=1
-                for i in range(j):
-                    try:
-                        leg = breeze.get_option_chain_quotes(stock_code="CNXBAN",
-                                                                exchange_code="NFO",
-                                                                product_type="options",
-                                                                expiry_date=f'{expiry}T06:00:00.000Z',
-                                                                right="call",
-                                                                strike_price=atm)
-                        if leg['Status']==200:
-                            leg = leg['Success']
-                            leg = pd.DataFrame(leg)
-                            buy_price = float(leg['ltp'][0])
-                            
-                            print(now, 'buy', atm, 'call at:', buy_price)
-                            break
-                        else:
-                            j+=1
-                    except:
-                        j+=1
-                        time.sleep(5)
+                print(atm)
+                initiate_ws(atm,'call')
+                # try:
+                time.sleep(2)
+                buy_price=float(one_tick['last'])
+                # except:
+                # leg = breeze.get_option_chain_quotes(stock_code="CNXBAN",
+                #                                     exchange_code="NFO",
+                #                                     product_type="options",
+                #                                     expiry_date=f'{expiry}T06:00:00.000Z',
+                #                                     right="call",
+                #                                     strike_price=atm)
+                # leg = leg['Success']
+                # leg = pd.DataFrame(leg)
+                # buy_price = float(leg['ltp'][0])
                 
-                
-            elif last_row['RSI_14']<30 and second_last['RSI_14']<30 and last_row['MACD']<second_last['MACD'] and second_last['MACD']<third_last['MACD'] :
-                entry_time = datetime.now().strftime('%H:%M:%S')
-                order = -1
+                sl=buy_price-stoploss
+                # deactivate_ws(atm,'call')
+                print(now, 'buy', atm, 'call at:', buy_price)
+
+            elif last_row['RSI_14']<30 and second_last['RSI_14']<30 and last_row['MACD']<second_last['MACD']<third_last['MACD']:
+                entry_time=datetime.now().strftime('%H:%M:%S')
+                order=-1
                 atm = round(last_row['close'] / 100) * 100
-                j=1
-                for i in range(j):
-                    try:
-                        leg = breeze.get_option_chain_quotes(stock_code="CNXBAN",
-                                                                exchange_code="NFO",
-                                                                product_type="options",
-                                                                expiry_date=f'{expiry}T06:00:00.000Z',
-                                                                right="put",
-                                                                strike_price=atm)
-                        
-                        if leg['Status']==200:
-                            leg = leg['Success']
-                            leg = pd.DataFrame(leg)
-                            buy_pe_price = float(leg['ltp'][0])
-                            
-                            print(now, 'buy', atm, 'put at:', buy_pe_price)
-                            break
-                        else:
-                            j+=1
-                    except:
-                        j+=1
-                        time.sleep(5)
+                initiate_ws(atm,'put')
+                time.sleep(5)
+                buy_price=float(one_tick['last'])
+                sl=buy_price-stoploss
+                # deactivate_ws(atm,'put')
+                print(now, 'buy', atm, 'put at:', buy_price)
             else:
                 print(last_row['datetime'], 'no trade condition: rsi is ', last_row['RSI_14'], 'macd is ', last_row['MACD'])
-                    
+    if order==1 and last_row is not None:
+        premium=one_tick['last']
+        
+        if premium >= buy_price-sl:
+                # old_sl=sl
+                sl = adjust_trailing_sl(premium, sl, order)
+                print(f"Stop Loss trailed. Premium: {premium}, New SL: {sl}")
+                # logging.info(f"Stop Loss trailed. Premium: {one_tick['last']}, New SL: {sl}")
                 
-        if order == 1 :
-            if last_row['MACD'] < second_last['MACD'] and second_last['MACD'] < third_last['MACD'] :
-                order = 0
-                exit_time = datetime.now().strftime('%H:%M:%S')
-                
-                j=1
-                for i in range(j):
-                    try:
-                        leg = breeze.get_option_chain_quotes(stock_code="CNXBAN",
-                                                                exchange_code="NFO",
-                                                                product_type="options",
-                                                                expiry_date=f'{expiry}T06:00:00.000Z',
-                                                                right="call",
-                                                                strike_price=atm)
-                        if leg['Status']==200:
-                            leg = leg['Success']
-                            leg = pd.DataFrame(leg)
-                            sell_price = float(leg['ltp'][0])
-                            
-                            pnl = round(sell_price - buy_price, 2)
-                            print(now, 'exit', atm, 'call pnl is:', pnl)
-                            break
-                        else:
-                            j+=1
-                    except:
-                        j+=1
-                        time.sleep(5)
-                
+        if last_row['MACD'] < second_last['MACD'] and second_last['MACD'] < third_last['MACD'] and premium<=sl :
+            order = 0
+            # initiate_ws(atm,'call')
+            exit_time = datetime.now().strftime('%H:%M:%S')
+            if one_tick is not None:
+                sell_price=float(premium)
+                pnl = round(sell_price - buy_price, 2)
+                print(now, 'exit', atm, 'call pnl is:', pnl)
+                deactivate_ws(atm,right='call')
+                write_data=pd.DataFrame([[today, entry_time, atm, 'call', buy_price, exit_time, sell_price, pnl]],columns=['Date', 'Entry Time', 'Strike', 'CE or PE', 'Entry premium','Exit Time', 'Exit premium', 'PnL'])
+
                 csv_file = "Paper_trade_BN_macd.csv"
-                try:
-                    with open(csv_file, 'x', newline='') as file:
-                        writer = csv.writer(file)
-                        writer.writerow(['Date', 'Entry Time', 'Strike', 'CE or PE', 'Entry premium','Exit Time', 'Exit premium', 'PnL'])
-                except FileExistsError:
-                    pass
-                    with open(csv_file, 'a', newline='') as file:
-                        writer = csv.writer(file)
-                        writer.writerow([today, entry_time, atm, 'call', buy_price, exit_time, sell_price, pnl])
-                
-                
-            else:
-                print(last_row['datetime'], 'no exit condition: rsi is ', last_row['RSI_14'], 'macd is ', last_row['MACD'])
-                
-        if order == -1 :
-            if last_row['MACD'] > second_last['MACD'] and second_last['MACD'] > third_last['MACD'] :
-                order = 0
-                exit_time = datetime.now().strftime('%H:%M:%S')
-                j=1
-                for i in range(j):
-                    try:
-                        leg = breeze.get_option_chain_quotes(stock_code="CNXBAN",
-                                                                exchange_code="NFO",
-                                                                product_type="options",
-                                                                expiry_date=f'{expiry}T06:00:00.000Z',
-                                                                right="put",
-                                                                strike_price=atm)
-                        if leg['Status']==200:
-                            leg = leg['Success']
-                            leg = pd.DataFrame(leg)
-                            sell_pe_price = float(leg['ltp'][0])
-                            
-                            pnl = round(sell_pe_price - buy_pe_price, 2)
-                            print(now, 'exit', atm, 'put pnl is:', pnl)
-                            break
-                        else:
-                            j+=1
-                    except:
-                        j+=1
-                        time.sleep(5)
+                if os.path.exists(csv_file):
+                        write_data.to_csv(csv_file,header=False,mode='a',index=False)
+
+                else:
+                    write_data.to_csv(csv_file,header=True,index=False)
+    if order==-1 and last_row is not None:
+        premium=one_tick['last']
+        if premium >= buy_price-sl:
+                sl = adjust_trailing_sl(premium, sl, order)
+                print(f"Stop Loss trailed. Premium: {premium}, New SL: {sl}")
+        if last_row['MACD'] > second_last['MACD'] and second_last['MACD'] > third_last['MACD'] and premium<=sl:
+            order = 0
+            # initiate_ws(atm,right='put')
+            exit_time = datetime.now().strftime('%H:%M:%S')
+            if one_tick is not None:
+                sell_price=float(premium)
+                pnl = round(sell_price - buy_price, 2)
+                print(now, 'exit', atm, 'put pnl is:', pnl)
+                deactivate_ws(atm,right='put')
+                write_data=pd.DataFrame([[today, entry_time, atm, 'call', buy_price, exit_time, sell_price, pnl]],columns=['Date', 'Entry Time', 'Strike', 'CE or PE', 'Entry premium','Exit Time', 'Exit premium', 'PnL'])
+
                 csv_file = "Paper_trade_BN_macd.csv"
-                try:
-                    with open(csv_file, 'x', newline='') as file:
-                        writer = csv.writer(file)
-                        writer.writerow(['Date', 'Entry Time', 'Strike', 'CE or PE', 'Entry premium','Exit Time', 'Exit premium', 'PnL'])
-                except FileExistsError:
-                    pass
-                    with open(csv_file, 'a', newline='') as file:
-                        writer = csv.writer(file)
-                        writer.writerow([today, entry_time, atm, 'put', buy_pe_price, exit_time, sell_pe_price, pnl])
-                    
-            else:
+                if os.path.exists(csv_file):
+                        write_data.to_csv(csv_file,header=False,mode='a',index=False)
+                else:
+                    write_data.to_csv(csv_file,header=True,index=False)
+        else:
                 print(last_row['datetime'], 'no exit conditon: rsi is ', last_row['RSI_14'], 'macd is ', last_row['MACD'])
-                
-        time.sleep(5)
-                
-                
+    
+    if (time_1 < t(datetime.now().time().hour, datetime.now().time().minute) > time_2):
+        breeze.ws_disconnect()
+        quit()
+        # print("----------------------------------------")      
+    time.sleep(1)
