933235c56b16b6e09aefc8a813aea2dd8c7cc862
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-11-29 09:26:23 +0530

    Update breeze1.py

--- breeze1.py
+++ breeze1.py
@@ -2,4 +2,4 @@ from breeze_connect import BreezeConnect
 breeze = BreezeConnect(api_key="d96783Qp368558*55FI36Z24W0ET39Lf")
 import urllib
 breeze.generate_session(api_secret="58836g597W4l8977h7~%eX9967^807x3",
-                        session_token="49425866")
+                        session_token="49454102")
