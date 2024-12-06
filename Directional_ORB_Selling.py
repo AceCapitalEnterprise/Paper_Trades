07ffce1e5dce27183807f9f7d519547635b3f77b
AceCapitalEnterprise <acecapitalent@gmail.com>
2024-12-06 09:36:33 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -21,8 +21,8 @@ time_1 = t(9, 30)
 time_2 = t(15, 30)
 order = 0
 atm_strike=None
-expiry = '2024-12-05'
-expiry1 = '05-Dec-2024'
+expiry = '2024-12-12'
+expiry1 = '12-Dec-2024'
 fut_expiry = '2024-12-26'
 
 SL = 5
