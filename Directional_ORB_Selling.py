8c13b1a61f7789f6df17e3805daf9c4a0f14f8b7
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-01-17 14:57:12 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -635,8 +635,8 @@ while True:
     if not positions_df_pe.empty:
         import time,os
         positions_df_pe = update_trailing_sl(positions_df_pe,path_pe)
-        print(positions_df_pe)
-        # positions_df_pe = check_profit_target_and_add_position(positions_df_pe,path_pe)
+        # print(positions_df_pe)
+        positions_df_pe = check_profit_target_and_add_position(positions_df_pe,path_pe)
         if now.time() >= t(15, 30):
             path_pe="unclosed_positions_directional_pe.csv"
             # if os.path.exists(path):
@@ -653,8 +653,8 @@ while True:
     if not positions_df_ce.empty:
         import time,os
         positions_df_ce = update_trailing_sl(positions_df_ce,path_ce)
-        print(positions_df_ce)
-        # positions_df_ce = check_profit_target_and_add_position(positions_df_ce,path_ce)
+        # print(positions_df_ce)
+        positions_df_ce = check_profit_target_and_add_position(positions_df_ce,path_ce)
         if now.time() >= t(15, 30):
             path_ce="unclosed_positions_directional_ce.csv"
             # if os.path.exists(path):
