5575fb764e24d1dad78265f1a0524e14f437708c
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-02-21 09:41:43 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -1,7 +1,7 @@
 from breeze_connect import BreezeConnect
 breeze = BreezeConnect(api_key="d96783Qp368558*55FI36Z24W0ET39Lf")
 breeze.generate_session(api_secret="58836g597W4l8977h7~%eX9967^807x3",
-                        session_token="50666268")
+                        session_token="50679933")
 
 import numpy as np
 import pandas as pd
@@ -18,8 +18,8 @@ time_1 = t(9, 30)
 time_2 = t(15, 30)
 order = 0
 atm_strike=None
-expiry = '2025-02-20'      
-expiry1 = '20-Feb-2025'
+expiry = '2025-02-27'      
+expiry1 = '27-Feb-2025'
 fut_expiry = '2025-02-27'
 adding_pos = True
 
