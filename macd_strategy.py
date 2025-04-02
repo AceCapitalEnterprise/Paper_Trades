407c2c5eeec6189c8dd8879d80b62c8d50280d08
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-04-02 09:48:07 +0530

    Update macd_strategy.py

--- macd_strategy.py
+++ macd_strategy.py
@@ -24,7 +24,7 @@ try:
     breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
     breeze.generate_session(
         api_secret="9331K77(I8_52JG2K73$5438q95772j@",
-        session_token="51054448"
+        session_token="51067353"
     )
     print("BreezeConnect initialized successfully")
 except Exception as e:
