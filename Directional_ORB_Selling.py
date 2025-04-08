730e8a568d85805abbca8c59ce46a62f1166d954
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-04-08 10:21:28 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -40,7 +40,7 @@ except Exception as e:
 # Constants
 TIME_1 = datetime.strptime("09:15", "%H:%M").time()
 TIME_2 = datetime.strptime("15:20", "%H:%M").time()
-EXPIRY = '2025-04-03'
+EXPIRY = '2025-04-09'
 QTY = 225
 
 atm_strike=None
