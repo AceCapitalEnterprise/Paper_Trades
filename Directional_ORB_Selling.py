76f6b23048c1c5e26100c956c84a69ff64eba9b9
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-04-21 10:23:44 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -29,7 +29,7 @@ try:
     breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
     breeze.generate_session(
         api_secret="9331K77(I8_52JG2K73$5438q95772j@",
-        session_token="51219928"
+        session_token="51242818"
     )
     print("BreezeConnect initialized successfully")
 except Exception as e:
@@ -40,7 +40,7 @@ except Exception as e:
 # Constants
 TIME_1 = datetime.strptime("09:15", "%H:%M").time()
 TIME_2 = datetime.strptime("15:20", "%H:%M").time()
-EXPIRY = '2025-04-17'
+EXPIRY = '2025-04-24'
 QTY = 225
 
 atm_strike=None
