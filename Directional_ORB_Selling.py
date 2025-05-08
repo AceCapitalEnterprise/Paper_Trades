d6517c3d03a30cde75d8630aea59041fd0c7ddaf
AceCapitalEnterprise <acecapitalent@gmail.com>
2025-05-08 09:45:27 +0530

    Update Directional_ORB_Selling.py

--- Directional_ORB_Selling.py
+++ Directional_ORB_Selling.py
@@ -29,7 +29,7 @@ try:
     breeze = BreezeConnect(api_key="77%U3I71634^099gN232777%316Q~v4=")
     breeze.generate_session(
         api_secret="9331K77(I8_52JG2K73$5438q95772j@",
-        session_token="51399273"
+        session_token="51423088"
     )
     print("BreezeConnect initialized successfully")
 except Exception as e:
